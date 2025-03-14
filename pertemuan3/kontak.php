5f354f2e97755ce2911063985ab14754dbe90333
nawafrafif <yahyakhansa92@gmail.com>
2025-03-14 08:03:24 +0700

    seadanya

--- pertemuan3/kontak.php
+++ pertemuan3/kontak.php
@@ -9,9 +9,9 @@ include 'templet/menu.php';
             ?>
                 <!-- Page content-->
                 <div class="container-fluid d-flex justify-content-center">
-                   <img src="img/dc.png" style="width : 80px; heigt : 10px"  alt="">
-                   <img src="img/ig.png" style="width : 150px"  alt="">
-                   <img src="img/wa.png" style="width : 150px" alt="">
+                   <a href="https://discord.com/channels/@me "><img src="img/dc.png" style="width : 150px; height:auto;" class="mt-3" alt=""></a>
+                   <a href="https://www.instagram.com/mekgumikatou/"><img src="img/ig.png" style="width : 140px"  alt=""></a>
+                   <a href="https://api.whatsapp.com/send?phone=+6285691549775&text=halo"><img src="img/wa.png" style="width : 150px" alt=""></a>
                 </div>
             </div>
         </div>
